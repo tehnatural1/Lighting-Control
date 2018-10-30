c31a9c21006a9d86c7668513bac9afa0e1c7c26f
tehnatural <dholl086@uOttawa.ca>
2018-10-30 13:49:09 -0400

    remove unused code and cleanup

--- GPIOControl.c
+++ GPIOControl.c
@@ -8,7 +8,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#define PAGE_SIZE (4*1024)
+
 #define BLOCK_SIZE (4*1024)
 
 int  mem_fd;
@@ -21,25 +21,14 @@ volatile unsigned *gpio;
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
 #define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
 #define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
 
 #define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
 
-#define GPIO_PULL *(gpio+37) // Pull up/pull down
-#define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
 
 void setup_io();
-
-void printButton(int g)
-{
-  if (GET_GPIO(g)) // !=0 <-> bit is 1 <- port is HIGH=3.3V
-    printf("Button pressed!\n");
-  else // port is LOW=0V
-    printf("Button released!\n");
-}
 
 int main(int argc, char **argv)
 {
@@ -80,7 +69,7 @@ int main(int argc, char **argv)
 
   return 0;
 
-} // main
+}
 
 
 //
@@ -88,30 +77,33 @@ int main(int argc, char **argv)
 //
 void setup_io()
 {
-   /* open /dev/mem */
-   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-      printf("can't open /dev/mem \n");
-      exit(-1);
-   }
+  /* open /dev/mem */
+  if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+    printf("can't open /dev/mem \n");
+    exit(-1);
+  }
 
-   /* mmap GPIO */
-   gpio_map = mmap(
-      NULL,             //Any adddress in our space will do
-      BLOCK_SIZE,       //Map length
-      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-      MAP_SHARED,       //Shared with other processes
-      mem_fd,           //File to map
-      GPIO_BASE         //Offset to GPIO peripheral
-   );
+  /* mmap GPIO */
+  gpio_map = mmap(
+    NULL,             //Any adddress in our space will do
+    BLOCK_SIZE,       //Map length
+    PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+    MAP_SHARED,       //Shared with other processes
+    mem_fd,           //File to map
+    GPIO_BASE         //Offset to GPIO peripheral
+  );
 
-   close(mem_fd); //No need to keep mem_fd open after mmap
+  close(mem_fd); //No need to keep mem_fd open after mmap
 
-   if (gpio_map == MAP_FAILED) {
-      printf("mmap error %d\n", (int)gpio_map);//errno also set!
-      exit(-1);
-   }
+  if (gpio_map == MAP_FAILED) {
+    printf("mmap error %d\n", (int)gpio_map); //errno also set!
+    exit(-1);
+  }
 
-   // Always use volatile pointer!
-   gpio = (volatile unsigned *)gpio_map;
-
+  // Always use volatile pointer!
+  gpio = (volatile unsigned *)gpio_map;
 }
+
+
+
+
