37584c13a13cb36982241faf02df4b214c16c051
tehnatural <dholl086@uOttawa.ca>
2018-10-30 14:40:09 -0400

    more cleanup, and test of gpio toggle

--- GPIOControl.c
+++ GPIOControl.c
@@ -6,68 +6,53 @@
 
 #include "GPIOControl.h"
 
+
 int main(int argc, char **argv)
 {
-  int g,rep;
 
-  // Set up gpi pointer for direct register access
+  /* Set up gpio pointer for direct register access */
   setup_io();
 
-  // Set GPIO pins 7-11 to output
-  for (g=7; g<=11; g++)
-  {
-    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
-    OUT_GPIO(g);
-  }
-
-  for (rep=0; rep<10; rep++)
-  {
-     for (g=7; g<=11; g++)
-     {
-       SET_GPIO = 1<<g;
-       sleep(1);
-     }
-     for (g=7; g<=11; g++)
-     {
-       CLR_GPIO = 1<<g;
-       sleep(1);
-     }
-  }
+  toggle_gpio(27);
 
   return 0;
 
 }
 
 
-//
-// Set up a memory regions to access GPIO
-//
+/* 
+ * Set up memory regions to access GPIO
+ *
+ */
 void setup_io()
 {
   /* open /dev/mem */
-  if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+  if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
+  {
     printf("can't open /dev/mem \n");
     exit(-1);
   }
 
   /* mmap GPIO */
   gpio_map = mmap(
-    NULL,             //Any adddress in our space will do
-    BLOCK_SIZE,       //Map length
-    PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-    MAP_SHARED,       //Shared with other processes
-    mem_fd,           //File to map
-    GPIO_BASE         //Offset to GPIO peripheral
+    NULL,                 // Any adddress in our space will do
+    BLOCK_SIZE,           // Map length
+    PROT_READ|PROT_WRITE, // Enable reading & writting to mapped memory
+    MAP_SHARED,           // Shared with other processes
+    mem_fd,               // File to map
+    GPIO_BASE             // Offset to GPIO peripheral
   );
 
-  close(mem_fd); //No need to keep mem_fd open after mmap
+  /* No longer need to keep mem_fd after mmap */
+  close(mem_fd);
 
-  if (gpio_map == MAP_FAILED) {
+  if (gpio_map == MAP_FAILED)
+  {
     printf("mmap error %d\n", (int)gpio_map); //errno also set!
     exit(-1);
   }
 
-  // Always use volatile pointer!
+  /* Always use volatile pointer */
   gpio = (volatile unsigned *)gpio_map;
 }
 
