18e57ddd8724da07e662a7856f5418423dd96d6b
tehnatural <dholl086@uOttawa.ca>
2018-10-31 19:34:10 -0400

    Added threading for connections, moved connection handling to a new function

--- GPIOControl.c
+++ GPIOControl.c
@@ -1,214 +1,444 @@
+
 #include <stdio.h>
-#include <stdlib.h>
+
+// Register access
 #include <fcntl.h>
+
+
+// String functions
+#include <string.h>
+#include <stdlib.h>
+
+
 #include <sys/mman.h>
-#include <unistd.h>
-
-/* Definitions of data types used in system calls */
-#include <sys/types.h>
-
-/* Definitions and structures required for websocket */
 #include <sys/socket.h>
 
-/* Constants and structures for internet domain addresses */
-#include <netinet/in.h>
+// Internet
+#include <arpa/inet.h>
+#include <unistd.h>     //write
 
-/* Include local definitions for GPIO control */
+// For threading requests (lpthread)
+#include <pthread.h>
+
+
+// Used to get local IP address
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+
+// Time
+#include <time.h>
+
+// Holding definitions
 #include "GPIOControl.h"
 
 
+void
+main()
+{
+  // Set up gpio pointer for register access
+  setup_io();
 
-int main(int argc, char **argv)
+  init();
+}
+
+
+/*
+ * INFO:
+ * Toggles the input and output of a specified GPIO
+ * by checking if the GPIO is currently outputting
+ * any voltage.
+ *
+ * PARAMETERS:
+ * g:
+ * Specifies the GPIO to toggle.
+ *
+ */
+void
+toggle_gpio(int g)
+{
+  /* Setting the GPIO state */
+  INP_GPIO(g);
+  OUT_GPIO(g);
+
+  /* Check if GPIO outputting voltage */
+  if (GET_GPIO(g) > 0)
+  {
+    GPIO_CLR = 1<<g;
+  }
+  else
+  {
+    GPIO_SET = 1<<g;
+  }
+}
+
+
+
+/*
+ * INFO:
+ * Set up a memory regions to access GPIO directly
+ *
+ */
+void
+setup_io()
 {
 
-  /* Set up gpio pointer for direct register access */
-  setup_io();
-  
-
-//WEBSOCKET CREATION
-
-  /* File descriptors, they store the values returned by
-   * the socket system call and accept the system call */
-  int sockfd, newsockfd;
-
-  /* Stores the size of the address of the client. This
-   * is needed for the accept system call. */
-  int clilen;
-
-  /* Structure containing an internet address. */
-  struct sockaddr_in serv_addr, cli_addr;
-
-  /* 
-   * socket:
-   * System call to create a new socket. 
-   * 
-   * arg1 - domain of the socket   
-   *        AF_UNIX for two processes which share a common file systen
-   *        AF_INET for any two hosts on the internet 
-   * arg2 - type of the socket 
-   *        SOCK_STREAM characters read in a continous stream
-   *        SOCK_DGRAM message is read in chunks
-   * arg3 - protocol
-   * 		0 operating system will choose the most appropriate protocol 
-   *   		  TCP for stream sockets
-   *   		  UDP for datagram sockets
-   *
-   */
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-
-  /* Set the address family */
-  serv_addr.sin_family = AF_INET;
-
-
-  /* htons: converts a port number to a network byte order */
-  serv_addr.sin_port = htons (PORT_NUMBER);
-
-
-  /* Sets the IP address of the host. */
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-
-  /* 
-   * bind:
-   * Binds a socket to an address, current host and
-   * port number on which the server will run.
-   *
-   * arg1 - Socket descriptor, address to which is bound
-   * arg2 - Pointer to structure of sockaddr, but serv_addr 
-   *        is of type sockaddr_in and so it is cast to 
-   *        the correct type.
-   * arg3 - Size of the server address.
-   *
-   */
-  if (bind(sockfd,
-           (struct sockaddr *) &serv_addr,
-           sizeof(serv_addr)) < 0)
+  // open /dev/mem REQUIRES SU
+  if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0)
   {
-    perror ("failed to bind");
-    return -1;
+    printf ("can't open /dev/mem \n");
+    exit(-1);
   }
 
 
-  /* 
-   * listen:
-   * Allows the process to listen on the socket for connections
-   * 
-   * arg1 - Socket file descriptor
-   * arg2 - The number of connections that can be waiting 
-   *        while the process is handling a connection 
-   *
-   */
-  listen (sockfd, 5);
+  // mmap GPIO
+  gpio_map = mmap(
+    NULL,                  // Any adddress in our space will do
+    BLOCK_SIZE,            // Map length
+    PROT_READ|PROT_WRITE,  // Enable reading & writting to mapped memory
+    MAP_SHARED,            // Shared with other processes
+    mem_fd,                // File to map
+    GPIO_BASE              // Offset to GPIO peripheral
+  );
 
 
-  /* Stores the size of the client address */
-  clilen = sizeof(cli_addr);
+  //No need to keep mem_fd open after mmap
+  close(mem_fd);
 
 
-  /* 
-   * accept: 
-   * System call causes the process to block until a client
-   * has successfully connected to the server. It returns a
-   * new file descriptor, all communication should be done
-   * using the new file descriptor.
-   *
-   * arg1 - Socket file descriptor
-   * arg2 - Reference pointer to the adress of the client, 
-   *        cli_addr is of type sockaddr_in and so it must
-   *        be cast to the correct type.
-   * arg3 - Reference pointer to size of the structure. 
-   *
-   * */
-  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  if (gpio_map == MAP_FAILED)
+  {
+    printf (
+      "mmap error %d\n",
+      (int)gpio_map
+    );
+
+    exit(-1);
+  }
+
+  // Using a volatile pointer
+  gpio = (volatile unsigned *)gpio_map;
+
+}
 
 
+/*
+ * INFO:
+ * Initialize a socket to accept incoming connections
+ * then trigger gpio events based on the message sent via
+ * the connection
+ *
+ */
+int
+init(void)
+{
+
+  int sock_desc, client_sock, c;
+
+  struct sockaddr_in server, client;
+  struct ifreq ifr;
 
 
+  // Create a socket
+  sock_desc = socket(
+    AF_INET,
+    SOCK_STREAM,
+    0
+  );
+
+  // Prepare the socket structure
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons(MESSAGE_PORT);
 
 
-//END OF WEBSOCKET CREATION
+  // Bind socket descriptor
+  if (
+    bind(
+      sock_desc,
+      (struct sockaddr *)&server,
+      sizeof(server)
+    ) < 0)
+  {
+    perror ("binding failed");
+    return 1;
+  }
+
+  listen(sock_desc, 3);
+
+
+  // Type of address to retrieve - IPv4 IP address
+  ifr.ifr_addr.sa_family = AF_INET;
+
+  // Copy the interface name in the ifreq structure
+  strncpy(
+    ifr.ifr_name,
+    "wlan0",
+    IFNAMSIZ - 1
+  );
+
+  ioctl(
+    sock_desc,
+    SIOCGIFADDR,
+    &ifr
+  );
+
+
+  // Displaying Server IP and Port
+  printf (
+    "\nLighting Server Setup - %s : %d\n",
+    inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr),
+    MESSAGE_PORT
+  );
+
+
+  printf("Waiting for connections\n");
+
+  // Accept an incoming connection
+  c = sizeof(struct sockaddr_in);
+
+  pthread_t thread_id;
+
+  while client_sock = accept(
+    sock_desc,
+    (struct sockaddr *)&client,
+    (socklen_t*)&c
+  )
+  {
+
+    // Logging IP of client connection
+    log_to_file(
+      "Client connected",
+      inet_ntoa(client.sin_addr)
+    );
+
+
+    // Create a thread to handle connection
+    if (
+      pthread_create(
+        &thread_id,
+        NULL,
+        new_conn,
+        (void*)&client_sock
+      ) < 0)
+    {
+      perror ("Failed to create a new thread.");
+      return 1;
+    }
+
+  }
+
+  if (client_sock < 0)
+  {
+    perror ("Failed to accept connection.");
+    return 1;
+  }
 
   return 0;
 
 }
 
 
-/* 
- * Set up memory regions to access GPIO
- *
- */
-void setup_io()
+void
+log_to_file(char *mess_desc, char *mess_item)
 {
-  /* open /dev/mem */
-  if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
-  {
-    printf("can't open /dev/mem \n");
-    exit(-1);
-  }
 
-  /* mmap GPIO */
-  gpio_map = mmap(
-    NULL,                 // Any adddress in our space will do
-    BLOCK_SIZE,           // Map length
-    PROT_READ|PROT_WRITE, // Enable reading & writting to mapped memory
-    MAP_SHARED,           // Shared with other processes
-    mem_fd,               // File to map
-    GPIO_BASE             // Offset to GPIO peripheral
+  char s[26];
+  struct tm* tm_info;
+
+  log_file = fopen("x.log", "a+");
+  time(&t);
+
+  tm_info = localtime(&t);
+
+  strftime(s, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+
+  // Writing log
+  fprintf(
+    log_file,
+    "[%s]: %s: %s\n",
+    s,
+    mess_desc,
+    mess_item
   );
 
-  /* No longer need to keep mem_fd after mmap */
-  close(mem_fd);
+  fclose(log_file);
 
-  if (gpio_map == MAP_FAILED)
-  {
-    //printf("mmap error %d\n", (int)gpio_map); //errno also set!
-    exit(-1);
-  }
+  memset(s, 0, sizeof(s));
 
-  /* Always use volatile pointer */
-  gpio = (volatile unsigned *)gpio_map;
+  // Clear memory
+  free(s)
+
 }
 
 
-/* 
- * NOTE:
- * Setup a websocket for communication to the server
- * the websocket expects a string corresponding to a
- * certain GPIO, which will be toggled.
- *
- */
-int setup_socket()
-{
-  int socket_desc, client_sock, c;
-}
 
-
-/* 
+/*
  * INFO:
- * Toggles the input and output of a specific GPIO
- * by checking the current state of the GPIO
+ * Handles each client connection and depending on the message
+ * recieved over the websocket the corresponding GPIO on the
+ * raspberry pi is triggered
  *
- * PARAMETERS:
- * in_gpio
- * [in] Specifies the GPIO to toggle 
- * 
  */
-void toggle_gpio(int in_gpio)
+void
+*new_conn(void *sock_desc)
 {
-  /* Set the GPIO to output */
-  INP_GPIO(in_gpio);
-  OUT_GPIO(in_gpio);
 
-  /* Check the GPIO current state and alternate it */
-  if (GET_GPIO(in_gpio) > 0)
+  // get socket descriptor
+  int sock = *(int*)sock_desc;
+  int msg_size;
+  char *message, client_message[2];
+
+  // Recieve message from a client
+  while (
+    msg_size = recv(
+      sock,
+      client_message,
+      2,
+      0
+    ) > 0)
   {
-    CLR_GPIO = 1 << in_gpio;
+
+    // End of string marker
+    client_message[msg_size] = '\0';
+
+    // Update log
+    log_to_file(
+      "Recieved client message",
+      client_message
+    );
+
+    /**/ if (strcmp(client_message, "4") == 0)
+    {
+      toggle_gpio(PATIO);
+    }
+    else if (strcmp(client_message, "5") == 0)
+    {
+      toggle_gpio(STAIRWAY);
+    }
+    else if (strcmp(client_message, "6") == 0)
+    {
+      toggle_gpio(LIVINGROOM);
+    }
+    else if (strcmp(client_message, "7") == 0)
+    {
+      toggle_gpio(HALLWAY);
+    }
+    else if (strcmp(client_message, "27") == 0)
+    {
+      toggle_christmas_lights();
+    }
+
+    // Zero memory
+    memset(client_message, 0, 2);
+
+    // Clear memory
+    free(client_message)
+
   }
-  else
+
+  /**/ if (msg_size == 0)
   {
-    SET_GPIO = 1 << in_gpio;
+
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+    int err = getpeername(
+      sock,
+      (struct sockaddr *)&addr,
+      &addr_len
+    );
+
+
+    if (err != 0)
+    {
+      // error
+    }
+
+    // Log IP of client disconnection
+    log_to_file(
+      "Client disconnected",
+      inet_ntoa(addr.sin_addr)
+    );
+
+
+    fflush(stdout);
+
+    free(addr)
+    free(addr_len)
+    free(err)
+
   }
+  else if (msg_size == -1)
+  {
+    perror ("recv failed.");
+  }
+
+
+  // Free Memory
+  free(message)
+  free(sock)
+  free(msg_size)
+
+  return 0;
 }
 
 
+int
+toggle_christmas_lights()
+{
+  int sock;
+  struct sockaddr_in xmas_pi;
+
+  // Create socket
+  sock = socket(
+    AF_INET,
+    SOCK_STREAM,
+    0
+  );
+
+
+  if (sock == -1)
+  {
+    printf("Could not create socket");
+    return 1;
+  }
+
+  xmas_pi.sin_addr.s_addr = inet_addr("192.168.1.76");
+  xmas_pi.sin_family = AF_INET;
+  xmas_pi.sin_port = htons(MESSAGE_PORT);
+
+  // Connect to other pi
+  if (
+    connect(
+      sock,
+      (struct sockaddr *)&xmas_pi,
+      sizeof(xmas_pi)
+    ) < 0)
+  {
+    perror("connect failed. Error");
+    return 1;
+  }
+
+  // Send message to other pi
+  if (
+    send(
+      sock,
+      "4",
+      strlen("4"),
+      0
+    ) < 0)
+  {
+    puts("Send failed");
+    return 1;
+  }
+
+  // Close socket
+  close(sock);
+
+  // Clear memory
+  free(sock);
+  free(xmas_pi);
+
+  return 0;
+}
--- GPIOControl.h
+++ GPIOControl.h
@@ -1,38 +1,65 @@
-/* Set for rpi3 model */
-#define BCM2708_PERI_BASE 0x3F000000
 
-/* GPIO controller */
+/*
+ * These definitions depend on the chipset of the
+ * raspberry pi, currently this is set to a model 3b+
+ *
+ */
+#define BCM2708_PERI_BASE 0x3F000000
 #define GPIO_BASE         (BCM2708_PERI_BASE + 0x200000)
 #define BLOCK_SIZE        (4 * 1024)
 
-/* GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) */
-#define INP_GPIO(g) *(gpio + ((g) / 10)) &= ~(7 << (((g) % 10) * 3))
-#define OUT_GPIO(g) *(gpio + ((g) / 10)) |=  (1 << (((g) % 10) * 3))
-#define GET_GPIO(g) (* (gpio + 13)&(1 << g))
 
-/* sets bits which are 1 and ignores bits which are 0 */
-#define SET_GPIO *(gpio + 7)
+/* Disables the ability of a GPIO to output voltage */
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 
-/* clears bits which are 1 and ignores bits which are 0 */
-#define CLR_GPIO *(gpio + 10)
+/* Allows the GPIO to output voltage */
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
 
-/* The port communications will be accepted on */
-#define PORT_NUMBER 9090
+/*
+ * Returns the current state of the GPIO
+ * 0, if no voltage is being output
+ * 1+, if voltage is currently being output on the GPIO
+ *
+ */
+#define GET_GPIO(g) (*(gpio+13)&(1<<g))
 
-/* used for opening '/dev/mem' */
-int mem_fd;
+/* Sets the GPIO to output voltage */
+#define GPIO_SET *(gpio+7)
 
-/* for mapping the device into memory */
-void *gpio_map;
+/* Sets the voltage output of the GPIO to 0 */
+#define GPIO_CLR *(gpio+10)
 
-/* sets up the gpio_map */
-void setup_io();
+/* GPIO pins are set to specific area of the house */
+#define LIVINGROOM  4
+#define STAIRWAY   23
+#define PATIO      24
+#define HALLWAY    25
 
-/* Alternates a GPIO's state */
+/* Port to accept communications on */
+#define MESSAGE_PORT 9090
+
+
+int init();
+
+/* toggles a pin on or off depending on it's current state */
 void toggle_gpio(int);
 
-/* I/O Access */
+void *new_conn(void *);
+
+/* Sets a point for direct register access */
+void setup_io();
+
+int mem_fd;
+
+void *gpio_map;
+
+/* I/O access */
 volatile unsigned *gpio;
 
+void log_to_file(char *, char *);
 
+FILE *log_file;
 
+time_t t;
+
+int toggle_christmas_lights();
